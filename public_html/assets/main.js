ccbce0f1ffde67a1c4361eb77d01b28423074fcc
root <root@kalimdor.vps.webforyou.tv>
2016-06-10 12:40:02 -0400

    Initial repository.

--- public_html/assets/main.js
+++ public_html/assets/main.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
     
+    /**
+     * Show / hide submit button.
+     */
     $('li.item').mouseover(function() {
         $(this).find('.hidden').addClass('visible').removeClass('hidden');
     }).mouseout(function() {
         $(this).find('.visible').removeClass('visible').addClass('hidden');
     });
     
+    /**
+     * Ajax.
+     */
     $('#likes').submit(function(e) {
         
         e.preventDefault();
@@ -17,16 +23,20 @@ $(document).ready(function() {
                 data: $(this).serialize()
             }
         }).done(function(data) {
-            var obj = jQuery.parseJSON(data);
             
+            var obj = jQuery.parseJSON(data);
             $.each(obj, function(key, val) {
                 var item = $('#items-wrapper').find('.likes[id=\'' + key + '\']');
                 item.html(parseInt(item.html()) + 1);
             });
-
+            
+            // Reset all the checkboxes.
             $('input[type=checkbox]').prop('checked', false);
             
+            // Shows the modal.
             $('#modal').fadeIn('fast');
+            
+            // Closes the modal.
             $('#modal-close').click(function() {
                 $('#modal').fadeOut('fast');
             });
